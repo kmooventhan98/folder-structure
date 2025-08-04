14928d5d4711e3347b7cc2b294ad7db90da036c4
mooventhan <mooventhan@ibm.com>
2025-08-04 23:29:07 +0530

    initial commit

---
+++ lib/folder/structure.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "structure/version"
+
+module Folder
+  module Structure
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/folder/structure_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Folder::Structure do
+  it "has a version number" do
+    expect(Folder::Structure::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
